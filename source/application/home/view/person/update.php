7e985bfbd2e93924f1add10f4383ca47f585deee
uercal <317215138@qq.com>
2019-09-20 14:16:00 +0800

    Signed-off-by: uercal <317215138@qq.com>

--- source/application/home/view/person/update.php
+++ source/application/home/view/person/update.php
@@ -1186,7 +1186,7 @@
                         <!-- PDF -->
                         <el-row type="flex" class="row-bg" style="background: #F4F6F2;padding:8px;ont-family: PingFangSC-Regular;font-size: 14px;color: #333333;letter-spacing: 0;">
                             <el-col :span="20" style="display:flex;align-items:center;">
-                                <label style="margin:0;">证件附件：<small style="color:#999999;font-weight:100;font-size:10px;">（将营业执照副本、资质资格资质、生产直销认可证或销售授权书等制成一个PDF文件上传，且不超过2MB）</small>
+                                <label style="margin:0;">证件附件：<small style="color:#999999;font-weight:100;font-size:10px;">（将营业执照副本、资质资格资信、生产直销许可证或销售授权书等制成一个PDF文件上传，且不超过2MB）</small>
                                 </label>
                             </el-col>
                         </el-row>
