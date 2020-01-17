4f115c5feea8beafa007a543348d7e9cdc7412cc
Uercal <317215138@qq.com>
2020-01-17 12:42:06 +0800

    Signed-off-by: Uercal <317215138@qq.com>

--- source/application/store/model/Activity.php
+++ source/application/store/model/Activity.php
@@ -59,9 +59,9 @@ class Activity extends ActivityModel
                 }
             }
             $data['sign_begin'] = strtotime($data['sign_begin']);
-            $data['sign_end'] = strtotime($data['sign_end'] . ' 23:59:59');
+            $data['sign_end'] = strtotime($data['sign_end']);
             $data['active_begin'] = strtotime($data['active_begin']);
-            $data['active_end'] = strtotime($data['active_end'] . ' 23:59:59');
+            $data['active_end'] = strtotime($data['active_end']);
             $this->allowField(true)->save($data);
             //
             Db::commit();
